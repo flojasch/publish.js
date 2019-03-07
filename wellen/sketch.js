a85cec886229d11f2dfb0b349a6df7521fc82801
flojasch <f.jasch@gmx.net>
2019-03-07 20:35:58 +0000

    doppler

--- wellen/sketch.js
+++ wellen/sketch.js
@@ -2,19 +2,36 @@ var renderer;
 var geometry;
 let traeger;
 let t = 0;
+let z = 0;
+let z0;
+let xPicked;
+let picked = false;
+let fSlider;
+let freq;
+let freqalt;
+let xh=0;
 
 function setup() {
-
     // we need to remember the renderer that is created so
     // we can call some of its internal methods later
     renderer = createCanvas(windowWidth, windowHeight, WEBGL);
+    fSlider = createSlider(0.02, 0.25, 0.02, 0.005);
+    fSlider.position(20, 20);
+    cSlider = createSlider(0.1, 0.5, 0.1, 0.05);
+    cSlider.position(200, 20);
+    sSlider = createSlider(0,1,0.5,0.05);
+    sSlider.position(400,20);
+    dampButton = createButton('damping');
+    dampButton.position(600, 20);
+    dampButton.mousePressed(toggleDamping);
+    text=createP("soundspeed",textSize=20);
+    text.position(200,40);
+    fill(0);
 
     noStroke();
-
     // set up the camera. the geometry is in the x,y plane
     // so the camera is below the z axis lookup up at (0,0,0)
     camera(0, -600, 300, 0, 0, 0, 0, -1, 0);
-
     // there's 10,000 points on the surface.
     geometry = new p5.Geometry(100, 100, function () {
         for (var y = 0; y <= this.detailY; y++) {
@@ -28,34 +45,78 @@ function setup() {
         }
     });
     traeger = new Traeger(geometry.detailX, geometry.detailY);
+    c = traeger.c;
+    freqalt = freq = 0;
+}
+
+function toggleDamping() {
+    if (traeger.damping == 0) {
+        traeger.damping = -2;
+    } else {
+        traeger.damping = 0;
+    }
+    traeger.reset();
+}
+
+function mousePressed() {
+    xPicked = floor(geometry.detailX / width * (100 + width - mouseX));
+    picked = true;
+    z0 = mouseY;
+    z = 0;
+
+}
+
+function mouseDragged() {
+    z = (z0 - mouseY) / height;
+    // console.log(xPicked,z);
+}
+
+function mouseReleased() {
+    picked = false;
 }
 
 function draw() {
     background(100);
-    for (var y = 0; y <= geometry.detailY; y++) {
-        for (var x = 0; x <= geometry.detailX; x++) {
+    traeger.c = cSlider.value();
+    freq = fSlider.value();
+    speed =sSlider.value();
+    if (freq != freqalt || traeger.c != c) {
+        t = 0;
+        freqalt = freq;
+        c = traeger.c;
+        traeger.reset();
+        // while (t < 500) {
+        //     traeger.update();
+        //     traeger.pull(geometry.detailX / 2, geometry.detailY / 2, 0.01 * Math.sin(t * freq));
+        //     t++;
+        // }
+    }
+    traeger.update();
+    // traeger.pull(geometry.detailX / 2, geometry.detailY / 2, 0.01 * Math.sin(t * freq));
+    t++;
+    if (picked) {
+        // traeger.set(xPicked, geometry.detailY / 2, z);
+    }
+    xh += speed;
+    if(xh>geometry.detailX){
+        xh=0;
+    }
+    traeger.pull(floor(xh), geometry.detailY / 2, 0.02 * Math.sin(t * freq));
+
+    for (let y = 0; y <= geometry.detailY; y++) {
+        for (let x = 0; x <= geometry.detailX; x++) {
             geometry.vertices[y * (geometry.detailX + 1) + x].z = traeger.z[x][y];
 
         }
     }
-    traeger.update();
-    t += 1;
-    // traeger.set(geometry.detailX / 2 - 10, geometry.detailY / 2, 0.05 * Math.sin(t / 2));
-    // traeger.set(geometry.detailX / 2 + 10, geometry.detailY / 2, 0.05 * Math.sin(t / 2));
     fill(255);
     noStroke();
-
-    // var dirX = (mouseX / width - 0.5) * 2;
-    // var dirY = (mouseY / height - 0.5) * 2;
-     console.log(mouseX,mouseY);
     directionalLight(66, 140, 244, 0, 0.5, 0.25);
-
     // re-compute the faces & normals
     geometry.computeFaces().computeNormals();
-
     // update the webgl buffers
     renderer.createBuffers("!", geometry);
-
     // render the geometry
     renderer.drawBuffersScaled("!", 1000, 1000, 500);
+
 }
--- wellen/wellentraeger.js
+++ wellen/wellentraeger.js
@@ -4,28 +4,38 @@ class Traeger {
         this.cols = cols;
         this.z = [];
         this.vz = [];
-        for (let i = 0; i <= rows; i++) {
+        this.damping = -2;
+        this.c=0.2;
+        this.reset();
+    }
+    reset(){
+        for (let i = 0; i <= this.rows; i++) {
             this.z[i] = [];
             this.vz[i] = [];
-            for (let j = 0; j <= cols; j++) {
+            for (let j = 0; j <= this.cols; j++) {
                 this.z[i][j] = 0;//0.5 * Math.exp(-0.1 * (pow(i - rows / 2,2) + pow(j - cols / 2,2)));
                 this.vz[i][j] = 0;
             }
         }
-
     }
+
     set(i, j, value) {
         this.z[i][j] = value;
+        this.vz[i][j] = 0;
     }
+
+    pull(i,j,force){
+        this.vz[i][j] +=force; 
+    }
+
     update() {
         let az = [];
-        let damping = -2.0;
         for (let i = 0; i <= this.rows; i++) {
             az[i] = [];
             for (let j = 0; j <= this.cols; j++) {
                 az[i][j] = 0;
                 if (j == this.cols||j==0||i==this.rows||i==0) {
-                    az[i][j] = damping * this.vz[i][j];
+                    az[i][j] = this.damping * this.vz[i][j];
                 } 
                 if (j < this.cols){
                     az[i][j] += this.z[i][j + 1] - this.z[i][j];
@@ -39,7 +49,7 @@ class Traeger {
                 if (i > 0) {
                     az[i][j] += this.z[i - 1][j]- this.z[i][j];
                 }
-                az[i][j] *= 0.2;
+                az[i][j] *= this.c;
             }
         }
 
