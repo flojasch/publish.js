466853a159ddf130d5974d84cc87699353f536f6
flojasch <f.jasch@gmx.net>
2019-04-06 15:18:32 +0000

    schwebung

--- Schwebung/sketch.js
+++ Schwebung/sketch.js
@@ -1,10 +1,14 @@
-
 let slider;
+let button;
+let update = true;
 
 function setup() {
   createCanvas(windowWidth, 400);
   slider = createSlider(0, 2, 1, 0.05);
   slider.position(20, 50);
+  button = createButton('start/stop');
+  button.position(220, 50);
+  button.mousePressed(onoff)
   let radius = 50;
   a1 = new Arrow(radius, 1, color(255, 255, 0));
   a2 = new Arrow(radius, 1, color(0, 255, 255));
@@ -12,17 +16,24 @@ function setup() {
   //frameRate(30);
 }
 
+function onoff() {
+  update = !update;
+}
+
 function draw() {
   background(0);
   translate(150, 200);
   a2.w = slider.value();
-  a3.w =a2.w;
-  a1.update();
-  a2.update();
-  a3.update();
+  a3.w = a2.w;
+  if (update) {
+    a1.update();
+    a2.update();
+    a3.update();
+  }
   a1.show();
   a2.show();
   a3.show(a1.x, a1.y);
+
 }
 
 class Arrow {
@@ -36,7 +47,7 @@ class Arrow {
     this.alpha = 0;
   }
   update() {
-    this.alpha += this.w*0.05;
+    this.alpha += this.w * 0.05;
     this.x = this.r * cos(this.alpha);
     this.y = this.r * sin(this.alpha);
   }
@@ -61,8 +72,9 @@ class Arrow {
 
     let x = this.x + tx;
     let y = this.y + ty;
-    
-    this.wave.unshift(y);
+    if (update) {
+      this.wave.unshift(y);
+    }
     push();
     translate(200, 0);
     stroke(255, 100);
