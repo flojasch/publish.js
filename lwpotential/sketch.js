63390ef8cd5758adc16103fdba928cb16d6b8d18
flojasch <f.jasch72@gmail.com>
2019-09-29 19:48:48 +0200

    lwpotential

--- lwpotential/sketch.js
+++ lwpotential/sketch.js
@@ -1,15 +1,17 @@
 let scale;
 let t = 0;
-let xext = 15;
+let t2 = 0;
+let xext = 30;
 let r0 = [];
 let charge;
-let c = 0.2;
+let c = 2;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   strokeWeight(1);
   scale = width / xext;
   charge = new Charge(0, 0, 1);
+  minus = new Charge(0, 0, -1);
 }
 
 function draw() {
@@ -18,36 +20,40 @@ function draw() {
   }
   background(200);
   translate(width / 2, height / 2);
-  vectors();
   let x = (mouseX - width / 2) / scale;
   let y = (mouseY - height / 2) / scale;
-  r0[t] = new p5.Vector(x, y);
-  charge.x = x;
-  charge.y = y;
-  alpha=0;
-  for (let n = 0; n < 10; n++) {
-    alpha += TWO_PI / 10;
-    x = charge.x + 0.1 * cos(alpha);
-    y = charge.y + 0.1 * sin(alpha);
-    fieldline(x, y);
+  charge.x = x;//sin(t2 * 0.1);
+  charge.y = y;//cos(t2*0.15);
+  r0[t] = new p5.Vector(charge.x, charge.y);
+  alpha = 0;
+  if (t > 10) {
+    vectors();
+    for (let n = 0; n < 20; n++) {
+      alpha += TWO_PI / 20;
+      x = charge.x + 0.1 * cos(alpha);
+      y = charge.y + 0.1 * sin(alpha);
+      fieldline(x, y);
+    }
   }
+  minus.show();
   charge.show();
-  if(t<300){
+  if (t < 300) {
     t++;
   } else {
-    r0.splice(0,1);
+    r0.splice(0, 1);
   }
+  t2++;
 }
 
 function fieldline(x, y) {
   let xalt, yalt;
-  for (let k = 0; k < 100; k++) {
+  for (let k = 0; k < 400; k++) {
     xalt = x;
     yalt = y;
-    let E=Efeld(x, y);
+    let E = Efeld(x, y);
     E.normalize();
-    x += E.x*0.1;
-    y += E.y*0.1;
+    x += E.x * 0.1;
+    y += E.y * 0.1;
     stroke(0, 0, 255);
     line(xalt * scale, yalt * scale, x * scale, y * scale);
   }
@@ -58,9 +64,9 @@ function vectors() {
     for (let j = 0; j < 50; j++) {
       let x = i * xext / 50 - xext / 2;
       let y = j * xext / 50 - xext / 2;
-      E=Efeld(x, y);
-      if (E.mag() < 1.5) {
-        let arrow = new Arrow(x * scale, y * scale, E.x * scale, E.y * scale);
+      E = Efeld(x, y);
+      if (E.mag() < 1) {
+        let arrow = new Arrow(x * scale, y * scale, E.x * scale*5, E.y * scale*5);
         arrow.show(color(255, 0, 0));
       }
     }
@@ -68,31 +74,28 @@ function vectors() {
 }
 
 function Efeld(x, y) {
-  let R=1;
-  let Rv=new p5.Vector(1,1);
-  let vv=new p5.Vector(1,1);
-  let v=1;
-  let a=new p5.Vector(1,1);
   let r = new p5.Vector(x, y);
-  for (let j = t; j > 0; j--) {
-    Rv = p5.Vector.sub(r, r0[j]);
-    R=Rv.mag();
+  for (let j = t; j >1; j--) {
+    let Rv = p5.Vector.sub(r, r0[j]);
+    let R = Rv.mag();
     if (R / c < t - j) {
-      vv=p5.Vector.sub(r0[j],r0[j-1]);
-      let v1=p5.Vector.sub(r0[j-1],r0[j-2]);
-      a=p5.Vector.sub(vv,v1);
-      a.div(c*c);
+      let vv = p5.Vector.sub(r0[j], r0[j - 1]);
+      // console.log(vv);
+      let v1 = p5.Vector.sub(r0[j - 1], r0[j - 2]);
+      let a = p5.Vector.sub(vv, v1);
+      // console.log(a);
+      a.div(c * c);
       vv.div(c);
-      break;
+      let v = vv.mag();
+      let Ev = p5.Vector.sub(Rv, p5.Vector.mult(vv,R));
+      Ev.mult(pow(R - p5.Vector.dot(vv, Rv), -3));
+      let Eb = p5.Vector.cross(Rv, p5.Vector.cross(Ev, a));
+      let res = p5.Vector.add(Ev.mult(1 - v * v), Eb);
+      //res.sub(r.mult(pow(r.mag(), -3)));
+      return res;
     }
   }
-  v=vv.mag();
-  vv.mult(R);
-  let Ev=p5.Vector.sub(Rv,vv);
-  Ev.mult(pow(R-p5.Vector.dot(vv,Rv),-3));
-  let Eb=p5.Vector.cross(Ev,a);
-  Eb=p5.Vector.cross(Rv,Eb);
-  return p5.Vector.add(Ev.mult(1-v*v),Eb);
+  return new p5.Vector(0.01,0.01);
 }
 
 class Charge {
