64d92ae82b888984db361bd75688234e702a9a45
flojasch <f.jasch72@gmail.com>
2019-11-01 11:23:36 +0100

    holografie

--- Holografie/sketch.js
+++ Holografie/sketch.js
@@ -1,40 +1,83 @@
-let R = 500;
+let R = 2;
 let slider;
 let lambda = 200;
 let brightness = [];
-let y0 = 500;
+let d;
+let points = [];
+
+class Point {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  slider = createSlider(1, 300, 50, 1);
+  slider = createSlider(1, 20, 1, 1);
   slider.position(50, 50);
-  lambslider = createSlider(1, 300, 100, 1);
+  lambslider = createSlider(1, 300, 20, 1);
   lambslider.position(50, 100);
   button = createButton("calcLight");
-  button.position(50, 200);
+  button.position(30, 150);
   button.mousePressed(setBrightness);
 }
 
+function mousePressed() {
+  if (mouseX > 200) {
+    points.push(new Point(mouseX, mouseY));
+  }
+}
+
+function isOpen(sX) {
+  let ret = false;
+  let x=points.length;
+  let y=0;
+  for (let point of points) {
+    let x0 = sX - point.x;
+    let y0 = point.y;
+    let alpha=TWO_PI / lambda * sqrt(y0 * y0 + x0 * x0);
+    x+=cos(alpha);
+    y+=sin(alpha);
+  }
+  if (x*x+y*y > points.length*points.length*1.8) {
+    ret = true;
+  }
+  return ret;
+}
+
 function setBrightness() {
-  let d = width / arrownum;
-  for (let i = 0; i < width; i++) {
-    for (let j = 0; j < height; j++) {
+  let dy, dx;
+  let d=10;
+  let max=0;
+  for (let i = 0; i < width; i += d) {
+    for (let j = 0; j < height; j += d) {
       let x = i;
       let y = j;
-      let arrnum=0;
-      for (let eX = 0; eX < width; eX += d) {
-        let x0=eX-width/2;
-        if (cos(TWO_PI / lambda * (sqrt(y0 * y0 + x0 * x0) - y0)) > 0) {
-          let dx = i - eX;
+      for (let sX = 0; sX < width; sX += d) {
+        if (isOpen(sX)) {
+          let dx = i - sX;
           let alpha = sqrt(j * j + dx * dx) / lambda * TWO_PI;
           x += cos(alpha);
           y += sin(alpha);
-          ++arrnum;
         }
       }
-      let dx = i - x;
-      let dy = j - y;
-      brightness[i + width * j] = 255/(arrnum*arrnum)  * (dx * dx + dy * dy);
+      dx = i - x;
+      dy = j - y;
+      let intensity=dx * dx + dy * dy;
+      if(intensity>max){
+        max=intensity;
+      }
+      for (let u = i; u < i + d; u++) {
+        for (let v = j; v < j + d; v++) {
+          brightness[u + width * v] = intensity;
+        }
+      }
+    }
+  }
+  for (let u = 0; u < width; u++) {
+    for (let v = 0; v < height; v++) {
+      brightness[u + width * v] *= 255/max;
     }
   }
 }
@@ -54,7 +97,7 @@ function draw() {
   if (width != windowWidth || height != windowHeight) {
     setup();
   }
-  arrownum = slider.value();
+  d = slider.value();
   lambda = lambslider.value();
 
   Arrows = [];
@@ -62,13 +105,10 @@ function draw() {
   setPixel();
   stroke(0, 0, 250);
   line(50, 120, 50 + lambda, 120);
-  let d = width / arrownum;
-  R = 1000 / arrownum;
   let x = mouseX;
   let y = mouseY;
   for (let eX = 0; eX < width; eX += d) {
-    let x0=eX-width/2;
-    if (cos(TWO_PI / lambda * (sqrt(y0 * y0 + x0 * x0) - y0)) > 0) {
+    if (isOpen(eX)) {
       let dx = mouseX - eX;
       let alpha = sqrt(mouseY * mouseY + dx * dx) / lambda * TWO_PI;
       let arrow = new Arrow(x, y, alpha);
