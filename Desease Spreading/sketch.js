22539e5ba2354fda036aaf358e32674b933fdd9a
flojasch <f.jasch72@gmail.com>
2020-05-02 12:02:38 +0200

    desease spreading

--- Desease Spreading/sketch.js
+++ Desease Spreading/sketch.js
@@ -12,8 +12,7 @@ function make2DArray(cols, rows) {
 let grid;
 let cols;
 let rows;
-let resolution = 5;
-let illdays = 2;
+let resolution=2;
 let healthy;
 let ill;
 let imun;
@@ -24,7 +23,7 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
   slider = createSlider(1, 30, 30, 1);
   slider.position(20, 10);
-  rslider = createSlider(3, 50, 10, 1);
+  rslider = createSlider(2, 50, 2, 1);
   rslider.position(170, 10);
   pslider = createSlider(0, 1, 0.5, 0.01);
   pslider.position(320, 10);
@@ -44,32 +43,15 @@ function reset() {
 
 function draw() {
   stroke(255);
-  //noStroke();
+  noStroke();
   frameRate(slider.value());
   if (pslider.value() != p) {
     p = pslider.value();
     reset();
   }
-  
   if (rslider.value() != resolution) {
     resolution = rslider.value();
     reset();
-  }
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      let x = i * resolution;
-      let y = j * resolution;
-      let state = grid[i][j];
-      if (state == 0) {
-        fill(200);
-      } else if (state < illdays && state > 0) {
-        fill(255, 0, 0);
-      } else if (state == illdays) {
-        fill(100);
-      }
-      rect(x, y, resolution , resolution );
-
-    }
   }
   imun = ill = healthy = 0;
   for (let i = 0; i < cols; i++) {
@@ -77,31 +59,30 @@ function draw() {
       let state = grid[i][j];
       if (state == 0) {
         healthy++;
-      } else if (state < illdays - 1) {
+        fill(200);
+      } else if (state == 1) {
         ill++;
-      } else {
-        imun++;
-      }
-      if (state == 0) {
+        fill(255, 0, 0);
+        next[i][j] = 2;
         for (let ih = -1; ih < 2; ih++) {
           for (let jh = -1; jh < 2; jh++) {
-            if (i + ih >= 0 && j + jh >= 0 && i + ih < cols && j + jh < rows && (ih == 0 || jh == 0)) {
-              let nstate = grid[i + ih][j + jh];
-              if (nstate > 0 && nstate < illdays) {
+            if (ih == 0 || jh == 0) {
+              let ni=(i+ih+cols)%cols;
+              let nj=(j+jh+rows)%rows;
+              let nstate = grid[ni][nj];
+              if (nstate == 0) {
                 if (Math.random() < p) {
-                  next[i][j] = 1;
+                  next[ni][nj] = 1;
                 }
               }
             }
           }
         }
-      } else if (state < illdays - 1) {
-        next[i][j] = state + 1;
-      } else if (state == illdays - 1) {
-        next[i][j] = state + 1;
-      } else {
-        next[i][j] = state;
+      } else if (state == 2) {
+        fill(100);
+        imun++;
       }
+      rect(i * resolution, j * resolution, resolution, resolution);
     }
   }
   for (let i = 0; i < cols; i++) {
@@ -127,4 +108,4 @@ function draw() {
   rect(b * t, 0, b, -imun * scale);
   translate(0, -imun * scale);
   t++;
-  }
+}
