e0fed4d0ee58f231937914ae8884e2c0bb776c80
flojasch <f.jasch72@gmail.com>
2020-11-08 14:30:53 +0100

    farbiges spektrum

--- farbigesSpektrum/sketch.js
+++ farbigesSpektrum/sketch.js
@@ -1,31 +1,72 @@
 let slider;
 let text;
+let weis=false;
 
 function setup() {
-  width=windowWidth;
-  height=windowHeight;
+  width = windowWidth;
+  height = windowHeight;
   createCanvas(width, height);
   background(0);
-  if(slider){ 
+  if (slider) {
     slider.remove();
     text.remove();
+    slider2.remove();
+    text2.remove();
   }
+  button = createButton('weiß');
+  button.position(550, 30);
+  button.mousePressed(toggleWeis);
   slider = createSlider(0, 0.5, 0.1, 0.01);
   slider.position(50, 50);
   text = createP();
-  text.position(80, 70);
+  text.position(50, 60);
   text.style('font-size', '130%');
   text.style('color', '#ffffff');
+  slider2 = createSlider(0, 0.5, 0.1, 0.01);
+  slider2.position(250, 50);
+  text2 = createP();
+  text2.position(250, 60);
+  text2.style('font-size', '130%');
+  text2.style('color', '#ffffff');
+  slider3 = createSlider(300, 900, 650, 1);
+  slider3.position(550, 50);
+  text3 = createP();
+  text3.position(550, 60);
+  text3.style('font-size', '130%');
+  text3.style('color', '#ffffff');
 }
 
-function setPixel(T) {
+function toggleWeis(){
+  weis=!weis;
+}
+
+function colorValues(x) {
+  let rm = 0;
+  let bm = 0;
+  let gm = 0;
+  for (let lambda = 300; lambda < 900; lambda++) {
+    let p = I(x, lambda);
+    let rgb = nmToRgb(lambda);
+    rm += p * rgb[0] / 250;
+    gm += p * rgb[1] / 250;
+    bm += p * rgb[2] / 250;
+  }
+
+  return [int(rm), int(gm), int(bm)];
+}
+
+function setPixel() {
   loadPixels();
 
   for (let i = 0; i < width; i++) {
-    lambda=650;
-    let x=(i-width/2); 
-    let p = I(x);
-    let rgb = nmToRgb(lambda);
+    let p=1;
+    let x = (i - width / 2);
+    if (weis) {
+      rgb = colorValues(x);
+    } else {
+      p = I(x, lambda);
+      rgb = nmToRgb(lambda);
+    }
     for (let j = 0; j < height; j++) {
       let k = j * width + i;
       pixels[k * 4] = rgb[0] * p;
@@ -38,11 +79,12 @@ function setPixel(T) {
 
 }
 
-function I(x){
-  if(x==0) return 1;
-  let b=PI*d*100/lambda*x;
-  let a=sin(b)/b;
-  return a*a;
+function I(x, lambda) {
+  if (x == 0) return 1;
+  let b = PI * d * 50 / lambda * x;
+  let a = sin(b) / b;
+  a *= cos(PI * g * 50 / lambda * x);
+  return a * a;
 
 }
 
@@ -52,7 +94,10 @@ function draw() {
   }
   background(0);
   d = slider.value();
-  setPixel(d);
-  text.html('Spaltbreite: '+ d+'mm');
-  
+  g = slider2.value() + d;
+  lambda = slider3.value();
+  setPixel(d, g);
+  text.html('Spaltbreite: ' + d + 'mm');
+  text2.html('Spaltmittenabstand: ' + int(g * 100) / 100 + 'mm');
+  text3.html('Wellenlänge: ' + lambda + 'nm');
 }
