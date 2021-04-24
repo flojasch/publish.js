14b61472870377d50b1bd354d010762a5ed54e19
flojasch <f.jasch72@gmail.com>
2021-04-24 17:15:58 +0200

    mandeldist

--- mandel/mandel.js
+++ mandel/mandel.js
@@ -1,51 +1,49 @@
-
-var mx = -0.7,my = 0.0;
-var ex = 2.8,ey = 2.8;
-var xmin,xmax,ymin,ymax;
-var colorgradSlider,maxiterSlider;
+var mx = -0.7,
+  my = 0.0;
+var ex = 2.8,
+  ey = 2.8;
+var xmin, xmax, ymin, ymax;
+var colorgradSlider, maxiterSlider;
 var dragx, dragy;
-var overscreen=false;
+var overscreen = false;
 
 
 function setup() {
   canvas = createCanvas(500, 500);
   canvas.position(50, 50);
   pixelDensity(1);
-  colorgradSlider= createSlider(0, 200,70, 1);
+  colorgradSlider = createSlider(0, 200, 70, 1);
   colorgradSlider.position(50, 550);
-  var colortxt=createDiv('Color');
+  var colortxt = createDiv('Color');
   colortxt.position(50, 570);
-  maxiterSlider=createSlider(0, 300, 50);
+  maxiterSlider = createSlider(0, 300, 50);
   maxiterSlider.position(350, 550);
-  var itertxt=createDiv('Iterations');
+  var itertxt = createDiv('Iterations');
   itertxt.position(350, 570);
 }
 
-function mousePressed()
-{
-	if (0 < mouseX && mouseX < width && 0 < mouseY && mouseY <height) { 
-		dragx = map(mouseX, 0, width, xmin, xmax);
-		dragy = map(mouseY, 0, height, ymax, ymin);
-		overscreen=true;
-	}
-}
-
-function mouseDragged()
-{
- if(overscreen){
-	mx -= map(mouseX, 0, width, xmin, xmax)-dragx;
-	my -= map(mouseY, 0, height, ymax, ymin)-dragy;
- }
-}
-
-function mouseReleased() {
-  if (overscreen) { 
-    overscreen=false;
+function mousePressed() {
+  if (0 < mouseX && mouseX < width && 0 < mouseY && mouseY < height) {
+    dragx = map(mouseX, 0, width, xmin, xmax);
+    dragy = map(mouseY, 0, height, ymax, ymin);
+    overscreen = true;
   }
 }
 
-function mouseWheel(event)
-{
+function mouseDragged() {
+  if (overscreen) {
+    mx -= map(mouseX, 0, width, xmin, xmax) - dragx;
+    my -= map(mouseY, 0, height, ymax, ymin) - dragy;
+  }
+}
+
+function mouseReleased() {
+  if (overscreen) {
+    overscreen = false;
+  }
+}
+
+function mouseWheel(event) {
   var zoom = 1.125;
   if (event.delta < 0) {
     zoom = 0.8;
@@ -57,83 +55,87 @@ function mouseWheel(event)
   ex *= zoom;
   ey *= zoom;
 
-  mx = ix - (ix-mx)*zoom;
-  my = iy - (iy-my)*zoom;
+  mx = ix - (ix - mx) * zoom;
+  my = iy - (iy - my) * zoom;
   return false;
 }
 
-function draw(){
+function draw() {
   loadPixels();
-  var maxiter=maxiterSlider.value();
-  var colorgrad=colorgradSlider.value();
+  var maxiter = maxiterSlider.value();
+  var colorgrad = colorgradSlider.value();
 
-  var x,y,cx,cy,n,xx,yy,s;
-  var a=1/50.0;
-  var b=1/log(2);
-  var nu,s,t;
-  var RADIUS=exp(50),R=1;
-  var phi=0,phialt=0;
-  var faktor=0.9;
-  var c=4*(1-faktor)/(2-faktor);
-  var farbe=[0,0,80, 200, 200, 200,  255, 150, 0,  0, 0, 80];
-  var rgb=new Array(3);
+  var x, y, cx, cy, n, xx, yy, s;
+  var a = 1 / 50.0;
+  var b = 1 / log(2);
+  var nu, s, t;
+  var RADIUS = exp(50),
+    R = 1;
+  var phi = 0,
+    phialt = 0;
+  var faktor = 0.9;
+  var c = 4 * (1 - faktor) / (2 - faktor);
+  var farbe = [0, 0, 80, 200, 200, 200, 255, 150, 0, 0, 0, 80];
+  var rgb = new Array(3);
 
-  xmin = mx - ex/2;
-  xmax = xmin+ex;
-  ymin = my - ey/2;
-  ymax = ymin+ey;
+  xmin = mx - ex / 2;
+  xmax = xmin + ex;
+  ymin = my - ey / 2;
+  ymax = ymin + ey;
 
-  for (var i=0; i< width; i++) {
-    for (var j=0; j <height; j++) {
-      cx =map(i, 0, width, xmin, xmax);
-      cy =map(j, 0, height, ymax, ymin);
-      n=1;
-      phi=0;
-      
-      x=xx=0;
-      y=yy=0;
-      
-      while(n<maxiter) {
-		phialt=phi;
-        phi*=faktor;
-        
-        y=2*x*y+cy;
-        x=xx-yy+cx;
-        yy=y*y;
-        xx=x*x;
-		R=xx+yy;
-		phi+=1-abs(x)*x/R;
-        if (R>RADIUS)
+  for (var i = 0; i < width; i++) {
+    for (var j = 0; j < height; j++) {
+      cx = map(i, 0, width, xmin, xmax);
+      cy = map(j, 0, height, ymax, ymin);
+      n = 1;
+      phi = 0;
+
+      x = xx = 0;
+      y = yy = 0;
+
+      while (n < maxiter) {
+        phialt = phi;
+        phi *= faktor;
+
+        y = 2 * x * y + cy;
+        x = xx - yy + cx;
+        yy = y * y;
+        xx = x * x;
+        R = xx + yy;
+        phi += 1 - abs(x) * x / R;
+        if (R > RADIUS)
           break;
         ++n;
       }
 
-      var pix =(i+j*width)*4;
-      pixels[pix+3]=255;
-        if (n<maxiter) {
-		 nu=b*log(a*log(R));
-		 s = (sqrt(n + 1-nu)*0.004*colorgrad)%3;
-		 t=(((1-nu)*phi+nu*phialt)*c)%2*2;
-		 if (t>2){t=4-t;}
-         var k = floor(s);
-         for (var l = 0; l < 3; l++) {
-          rgb[l] =farbe[3*k+l]+(s-k)*(farbe[3+3*k+l]-farbe[3*k+l]);
+      var pix = (i + j * width) * 4;
+      pixels[pix + 3] = 255;
+      if (n < maxiter) {
+        nu = b * log(a * log(R));
+        s = (sqrt(n + 1 - nu) * 0.004 * colorgrad) % 3;
+        t = (((1 - nu) * phi + nu * phialt) * c) % 2 * 2;
+        if (t > 2) {
+          t = 4 - t;
+        }
+        var k = floor(s);
+        for (var l = 0; l < 3; l++) {
+          rgb[l] = farbe[3 * k + l] + (s - k) * (farbe[3 + 3 * k + l] - farbe[3 * k + l]);
           if (t < 1)
-            pixels[pix+l]= floor((255 + t * (rgb[l] - 255)));
+            pixels[pix + l] = floor((255 + t * (rgb[l] - 255)));
           else
-            pixels[pix+l]= floor((2 - t) * rgb[l]);
+            pixels[pix + l] = floor((2 - t) * rgb[l]);
 
-         }
+        }
 
       } else {
-        pixels[pix+0]=0;
-        pixels[pix+1]=0;
-        pixels[pix+2]=0;
+        pixels[pix + 0] = 0;
+        pixels[pix + 1] = 0;
+        pixels[pix + 2] = 0;
 
       }
     }
   }
   updatePixels();
- // text("Iterations", maxiterSlider.x * 2 + maxiterSlider.width, 495);
- // text("Color", colorgradSlider.x * 2 + colorgradSlider.width, 465);
+  // text("Iterations", maxiterSlider.x * 2 + maxiterSlider.width, 495);
+  // text("Color", colorgradSlider.x * 2 + colorgradSlider.width, 465);
 }
--- mandeldist/mandel.js
+++ mandeldist/mandel.js
@@ -6,10 +6,13 @@ var xmin, xmax, ymin, ymax;
 var colorgradSlider, maxiterSlider;
 var dragx, dragy;
 var overscreen = false;
-var cx, cy;
+let cx, cy;
 const RADIUS = 1000;
-var darr=[];
-var action=true;
+var darr = [];
+var action = true;
+var rgb = [];
+let maxiter;
+
 
 function setup() {
   canvas = createCanvas(500, 500);
@@ -19,10 +22,20 @@ function setup() {
   colorgradSlider.position(50, 550);
   var colortxt = createDiv('Color');
   colortxt.position(50, 570);
-  maxiterSlider = createSlider(0, 300, 50);
+  maxiterSlider = createSlider(0, 1000, 50,10);
   maxiterSlider.position(350, 550);
   var itertxt = createDiv('Iterations');
   itertxt.position(350, 570);
+  selfrac = createSelect();
+  selfrac.position(500, 30);
+  selfrac.option('distance');
+  selfrac.option('classic')
+  selfrac.changed(mySelectEvent);
+
+}
+
+function mySelectEvent() {
+  action = true;
 }
 
 function mousePressed() {
@@ -37,13 +50,14 @@ function mouseDragged() {
   if (overscreen) {
     mx -= map(mouseX, 0, width, xmin, xmax) - dragx;
     my -= map(mouseY, 0, height, ymax, ymin) - dragy;
+    action = true;
   }
 }
 
 function mouseReleased() {
   if (overscreen) {
     overscreen = false;
-    action=true;
+    action = true;
   }
 }
 
@@ -61,76 +75,119 @@ function mouseWheel(event) {
 
   mx = ix - (ix - mx) * zoom;
   my = iy - (iy - my) * zoom;
-  action=true;
+  action = true;
+  return false;
 }
 
 function draw() {
   mandel();
 }
 
-function mandeldist() {
-  var maxiter = maxiterSlider.value();
+function setd(i, j) {
+  darr[i][j] = 0;
   var c2 = cx * cx + cy * cy;
-  if (256.0 * c2 * c2 - 96.0 * c2 + 32.0 * cx - 3.0 < 0.0) return 0.0;
-  if (16.0 * (c2 + 2.0 * cx + 1.0) - 1.0 < 0.0) return 0.0;
-  var xn = xsq = dx = 0;
-  var yn = ysq = dy = 0;
+  if (256.0 * c2 * c2 - 96.0 * c2 + 32.0 * cx - 3.0 < 0.0) return;
+  if (16.0 * (c2 + 2.0 * cx + 1.0) - 1.0 < 0.0) return;
 
-  for (n = 0; n < maxiter; ++n) {
-    dxh = dx;
-    dx = 2 * (xn * dx - yn * dy) + 1;
-    dy = 2 * (xn * dy + yn * dxh);
-    yn = 2 * yn * xn + cy;
-    xn = xsq - ysq + cx;
+  if (selfrac.value() == 'distance') {
+    let xn = xsq = dx = 0;
+    let yn = ysq = dy = 0;
+    for (let n = 0; n < maxiter; ++n) {
+      dxh = dx;
+      dx = 2 * (xn * dx - yn * dy) + 1;
+      dy = 2 * (xn * dy + yn * dxh);
+      yn = 2 * yn * xn + cy;
+      xn = xsq - ysq + cx;
 
-    xsq = xn * xn;
-    ysq = yn * yn;
-    R = xsq + ysq;
-    if (R > RADIUS) return Math.sqrt(R / (dx * dx + dy * dy)) * Math.log(R);
+      xsq = xn * xn;
+      ysq = yn * yn;
+      R = xsq + ysq;
+      if (R > RADIUS) {
+        darr[i][j] = Math.sqrt(R / (dx * dx + dy * dy)) * Math.log(R);
+        return;
+      }
+    }
   }
-  return 0.0;
+  if (selfrac.value() == 'classic') {
+    let x = 0,
+      xx = 0,
+      y = 0,
+      yy = 0;
+    for (n = 0; n < maxiter; n++) {
+      y = 2 * x * y + cy;
+      x = xx - yy + cx;
+      yy = y * y;
+      xx = x * x;
+      R = xx + yy;
+      if (R > RADIUS) {
+        darr[i][j] = Math.sqrt(n + 1 - log(log(R) / log(RADIUS)) / log(2));
+        return;
+      }
+    }
+  }
+}
+
+function setrgb(i, j) {
+  colorgrad = colorgradSlider.value();
+
+  if (selfrac.value() == 'distance') {
+    let d = Math.pow(10 * darr[i][j] / ex, 5/colorgrad);
+    d = Math.min(1, d) * 255;
+    for (let l = 0; l < 3; l++) {
+      rgb[l] = d;
+    }
+  }
+  if (selfrac.value() == 'classic') {
+    let farbe = [0, 0, 80, 200, 200, 200, 255, 150, 0, 0, 0, 80];
+    let s = (darr[i][j] * 0.004 * colorgrad) % 3;
+    var k = floor(s);
+    if (darr[i][j] != 0)
+      for (let l = 0; l < 3; l++)
+        rgb[l] = farbe[3 * k + l] + (s - k) * (farbe[3 + 3 * k + l] - farbe[3 * k + l]);
+    else
+      for (let l = 0; l < 3; l++)
+        rgb[l] = 0;
+  }
 }
 
 function mandel() {
   loadPixels();
-  var colorgrad = colorgradSlider.value();
+
+  if (maxiter != maxiterSlider.value()) {
+    maxiter = maxiterSlider.value();
+    action = true;
+  }
   xm = ym = exh = 0;
   xmin = mx - ex / 2;
   xmax = xmin + ex;
   ymin = my - ey / 2;
   ymax = ymin + ey;
 
-
   if (action) {
-    action=false;
+    action = false;
     for (var i = 0; i < width; i++) {
-      darr[i]=[];
+      darr[i] = [];
       for (var j = 0; j < height; j++) {
         cx = map(i, 0, width, xmin, xmax);
         cy = map(j, 0, height, ymax, ymin);
-        var d = mandeldist();
-        darr[i][j]=d;
-        d = Math.pow(8.  * colorgrad / 10* d / ex, 0.2);
-        d = Math.min(1, d) * 255;
+        setd(i, j);
+        setrgb(i, j);
         var pix = (i + j * width) * 4;
         pixels[pix + 3] = 255;
         for (var l = 0; l < 3; l++)
-          pixels[pix + l] = d;
+          pixels[pix + l] = rgb[l];
       }
     }
-  } else{
-    for (var i = 0; i < width; i++) {
-      for (var j = 0; j < height; j++) {
-        d = Math.pow(8. * colorgrad / 10 * darr[i][j] / ex, 0.2);
-        d = Math.min(1, d) * 255;
+  } else {
+    for (let i = 0; i < width; i++) {
+      for (let j = 0; j < height; j++) {
+        setrgb(i, j);
         var pix = (i + j * width) * 4;
         pixels[pix + 3] = 255;
         for (var l = 0; l < 3; l++)
-          pixels[pix + l] = d;
+          pixels[pix + l] = rgb[l];
       }
     }
   }
   updatePixels();
-  // text("Iterations", maxiterSlider.x * 2 + maxiterSlider.width, 495);
-  // text("Color", colorgradSlider.x * 2 + colorgradSlider.width, 465);
 }
